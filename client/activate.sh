df6b6cad99400b62682d5be2858fee8ce91caa83
MoyTW <moytravis@gmail.com>
2022-01-09 23:26:21 -0800

    retooling for my other computer

--- client/activate.sh
+++ client/activate.sh
@@ -1,1 +1,7 @@
-python3 -m http.server 8080
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  python3 -m http.server 8080
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  py -m http.server 8080
+fi
+
+
--- client/run.sh
+++ client/run.sh
@@ -1,3 +1,7 @@
 mkdir -p html
 tweego src -f sugarcube-2.36.1 -o ./html/tweego_run.html
-xdg-open http://localhost:8080/html/tweego_run.html
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  xdg-open http://localhost:8080/html/tweego_run.html
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  start http://localhost:8080/html/tweego_run.html
+fi
