33c14047d3dc83b43543ad800169f949beb600b0
MoyTW <moytravis@gmail.com>
2022-01-11 19:49:27 -0800

    Moving handlers into websockets.js
    So, basically, the problem here is that you can't be sure the players
    are synced up. One player can be on a different screen. So relying on
    passage ordering to register the handlers is a little dicey.
    
    So, let's just not do that. The MAJOR minus is that it decouples the
    handler pretty decisively from the passage, which makes the coding
    gnarly.

--- client/src/websockets.js
+++ client/src/websockets.js
@@ -13,10 +13,70 @@ setup.websocketConnect = function(sessionId) {
 
   setup.chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    setup.handlers[data['type']](data);
+    const handler = setup.handlers[data['type']]
+    console.log(data['type'], handler);
+    handler(data);
   };
 
   setup.chatSocket.onclose = function(e) {
     console.error('TODO: Handle appropriately');
   }
 }
+
+// #################### HANDLERS ####################
+setup.MessageTypes = {
+  CharacterSelect: 'CHARACTER_SELECT',
+  CharacterConfirm: 'CHARACTER_CONFIRM',
+}
+
+// ==================== SelectCharacter ====================
+// -------------------- CharacterSelect --------------------
+setup.sendCharacterSelected = function(character) {
+  if (character !== 'Antony' && character !== 'Cleopatra') {
+    console.error(character + ' is not an allowed character!');
+    return;
+  }
+  setup.chatSocket.send(JSON.stringify({
+    'type': setup.MessageTypes.CharacterSelect,
+    'clientId': State.variables.clientId,
+    'character': character
+  }));
+}
+
+setup.registerHandler(setup.MessageTypes.CharacterSelect, function(data) {
+  if (data.clientId === State.variables.clientId) {
+    State.variables.playerCharacterName = data.character;
+    $('#player-selection').text('Your have selected ' + data.character + '.')
+  } else {
+    State.variables.partnerCharacterName = data.character;
+    $('#partner-selection').text('Your partner has selected ' + data.character + '.')
+  }
+  // TODO: Synchronize the states
+  const playerCN = State.variables.playerCharacterName;
+  const partnerCN = State.variables.partnerCharacterName;
+  if (playerCN !== '' && partnerCN !== '' && playerCN != partnerCN) {
+    $('#confirm button').prop('disabled', false)
+  } else {
+    $('#confirm button').prop('disabled', true)
+  }
+})
+
+// -------------------- CharacterConfirm --------------------
+setup.sendCharacterConfirmed = function() {
+  setup.chatSocket.send(JSON.stringify({
+    'type': setup.MessageTypes.CharacterConfirm,
+    'clientId': State.variables.clientId
+  }));
+}
+
+// TODO: Cross-check selections & kick back if different
+setup.registerHandler(setup.MessageTypes.CharacterConfirm, function(data) {
+  if (data.clientId === State.variables.clientId) {
+    State.variables.selectCharacterPlayerConfirmed = true
+  } else {
+    State.variables.selectCharacterPartnerConfirmed = true
+  }
+  if (State.variables.selectCharacterPlayerConfirmed && State.variables.selectCharacterPartnerConfirmed) {
+    Engine.play('Ch1_CaseIntro1');
+  }
+})
