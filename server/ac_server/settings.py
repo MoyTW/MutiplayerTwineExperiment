111dd60ef2c9dd471f2230629773de8964057b25
MoyTW <moytravis@gmail.com>
2022-01-15 20:05:22 -0800

    Prepping for test deployment

--- server/ac_server/settings.py
+++ server/ac_server/settings.py
@@ -10,20 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# Secrets & Settings
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-6g2f8ble#**3o*uj=w#8w&yi07s(78j!=qtyr)dsp4&pii=i'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG'] == 'True'
 
 ALLOWED_HOSTS = []
 
